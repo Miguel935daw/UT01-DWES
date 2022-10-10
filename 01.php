fa3db71fc892147e556375adbd333fbd4d779ac2
Miguel Ángel <mreytor2711@g.educaand.es>
2022-10-10 12:42:04 +0200

    Primer commit

---
+++ 01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP. -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 01</title>
+</head>
+<body>
+    <?php
+        echo 'Miguel Ángel Reyes'
+        echo 'Adiós'
+    ?>
+</body>
+</html>
